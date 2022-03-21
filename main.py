0c77d2b060d269fa97b27a0c4e84dda34eb353b5
Harish Chauhan <harish.chauhan99@gmail.com>
2022-03-21 16:59:56 -0400

    Configs for kernels size

--- main.py
+++ main.py
@@ -1,122 +1,47 @@
 '''Train CIFAR10 with PyTorch.'''
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-
-import torchvision
-import torchvision.transforms as transforms
-
-import os
-import argparse
-
+import torch, torch.nn as nn, torch.optim as optim, torch.nn.functional as F, torch.backends.cudnn as cudnn  
+import torchvision, torchvision.transforms as transforms
+import os, argparse, yaml 
+from torch.utils.tensorboard import SummaryWriter
 from models import *
-from utils import progress_bar
-
-
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true',
-                    help='resume from checkpoint')
-args = parser.parse_args()
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
-
-# Model
-print('==> Building model..')
-# net = VGG('VGG19')
-# net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-# net = ShuffleNetV2(1)
-# net = EfficientNetB0()
-# net = RegNetX_200MF()
-net = SimpleDLA()
-net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
-
-if args.resume:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
 
 # Training
-def train(epoch):
+def train(epoch, config):
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
     correct = 0
     total = 0
+    train_losses = [] 
+    train_acc = [] 
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, targets)
         loss.backward()
+        if config["grad_clip"]: nn.utils.clip_grad_value_(net.parameters(), clip_value=config["grad_clip"]) 
         optimizer.step()
 
         train_loss += loss.item()
+        train_losses.append(train_loss)
         _, predicted = outputs.max(1)
         total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
+        correct += predicted.eq(targets).sum().item() 
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
-
-def test(epoch):
+        train_acc.append(100.*correct/total) 
+        # print('Batch_idx: %d | Train Loss: %.3f | Train Acc: %.3f%% (%d/%d)'% (batch_idx, train_loss/(batch_idx+1), 100.*correct/total, correct, total)) 
+    writer.add_scalar('Loss/train_loss', np.mean(train_losses), epoch) 
+    writer.add_scalar('Accuracy/train_accuracy', np.mean(train_acc), epoch) 
+    
+# Testing 
+def test(epoch, config, savename):
     global best_acc
     net.eval()
     test_loss = 0
+    test_losses = [] 
+    test_acc = [] 
     correct = 0
     total = 0
     with torch.no_grad():
@@ -126,29 +51,121 @@ def test(epoch):
             loss = criterion(outputs, targets)
 
             test_loss += loss.item()
+            test_losses.append(test_loss)
             _, predicted = outputs.max(1)
             total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            correct += predicted.eq(targets).sum().item() 
+            test_acc.append(100.*correct/total) 
+            # print('Batch_idx: %d | Test Loss: %.3f | Test Acc: %.3f%% (%d/%d)'% ( batch_idx, test_loss/(batch_idx+1), 100.*correct/total, correct, total)) 
+        writer.add_scalar('Loss/test_loss', np.mean(test_losses), epoch) 
+        writer.add_scalar('Accuracy/test_accuracy', np.mean(test_acc), epoch) 
 
     # Save checkpoint.
     acc = 100.*correct/total
-    if acc > best_acc:
+    if acc > best_acc: 
         print('Saving..')
         state = {
             'net': net.state_dict(),
             'acc': acc,
             'epoch': epoch,
+            'config': config
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.pth')
+        torch.save(state, os.path.join('./summaries/', savename, 'ckpt.pth'))
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
-    scheduler.step()
+if __name__ == '__main__': 
+
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+    parser.add_argument('--config', default='resnet_configs/resnet.yaml', type=str, help='path to config file for resnet architecture') 
+    parser.add_argument('--resnet_architecture', default='test', type=str, help='name of resnet architecture from config') 
+
+    args = parser.parse_args()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    best_acc = 0  # best test accuracy
+    start_epoch = 0  # start from epoch 0 or last checkpoint epoch 
+
+    # Model
+    print('==> Building model..')
+    config=None 
+    with open(args.config, "r") as stream:
+        try: config = yaml.safe_load(stream) 
+        except yaml.YAMLError as exc: print(exc) 
+
+    config=config[args.resnet_architecture]
+    
+    exp = args.resnet_architecture 
+
+    # Data
+    print('==> Preparing data..')
+    train_trans = [transforms.ToTensor()]
+    test_trans = [transforms.ToTensor()]
+    if config["data_augmentation"]: 
+        train_trans.append(transforms.RandomCrop(32, padding=4)) 
+        train_trans.append(transforms.RandomHorizontalFlip()) 
+    if config["data_normalize"]: 
+        train_trans.append(transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))) 
+        test_trans.append(transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))) 
+    transform_train = transforms.Compose(train_trans) 
+    transform_test = transforms.Compose(test_trans) 
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=config["batch_size"], shuffle=True, num_workers=config["num_workers"])
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=int(config["batch_size"]/4), shuffle=False, num_workers=config["num_workers"])
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    
+    # net = get_ResNet_default() 
+
+    net, total_params = get_ResNet(config=config) 
+    config['total_params'] = total_params 
+    print(net)
+    print('Total Parameters: ', total_params) 
+
+    if total_params > 5_000_000: 
+        print("===============================")
+        print("Total parameters exceeding 5M") 
+        print("===============================")
+        exit()
+    # exit()
+
+
+    net = net.to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+    if config["resume_ckpt"]: 
+       
+        # Load checkpoint.
+        print('==> Resuming from checkpoint..')
+        checkpoint = torch.load(config["resume_ckpt"])
+        net.load_state_dict(checkpoint['net'])
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+
+    criterion = nn.CrossEntropyLoss()
+
+    if config["optim"] == 'sgd': optimizer = optim.SGD(net.parameters(), lr=config["lr"], momentum=config["momentum"], weight_decay=config["weight_decay"]) 
+    if config["optim"] == 'adam': optimizer = optim.Adam(net.parameters(), lr=config["lr"], weight_decay=config["weight_decay"])
+
+    if config["lr_sched"] == 'CosineAnnealingLR': scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200) # Good 
+    if config["lr_sched"] == 'LambdaLR': scheduler =torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch: 0.65 ** epoch)
+    if config["lr_sched"] == 'MultiplicativeLR': scheduler =torch.optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lambda epoch: 0.65 ** epoch)
+    if config["lr_sched"] == 'StepLR': scheduler =torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1) 
+    if config["lr_sched"] == 'MultiStepLR': scheduler =torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[6,8,9], gamma=0.1) 
+    if config["lr_sched"] == 'ExponentialLR': scheduler =torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.1) 
+    if config["lr_sched"] == 'CyclicLR': scheduler =torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.001, max_lr=0.1,step_size_up=5,mode="triangular") 
+    if config["lr_sched"] == 'CyclicLR2': scheduler =torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.001, max_lr=0.1,step_size_up=5,mode="triangular2") 
+    if config["lr_sched"] == 'CyclicLR3': scheduler =torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.001, max_lr=0.1,step_size_up=5,mode="exp_range",gamma=0.85) 
+    if config["lr_sched"] == 'OneCycleLR': scheduler =torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.1, steps_per_epoch=10, epochs=10) 
+    if config["lr_sched"] == 'OneCycleLR2': scheduler =torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.1, steps_per_epoch=10, epochs=10,anneal_strategy='linear') 
+    if config["lr_sched"] == 'CosineAnnealingWarmRestarts': scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=1, eta_min=0.001, last_epoch=-1) 
+
+    writer = SummaryWriter('summaries/'+exp) 
+
+    for epoch in range(start_epoch, config["max_epochs"]): 
+        train(epoch, config) 
+        test(epoch, config, savename=exp) 
+        scheduler.step()
+    writer.close() 
--- models/resnet.py
+++ models/resnet.py
@@ -8,26 +8,27 @@ Reference:
 '''
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
+import torch.nn.functional as F 
+import numpy as np 
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, conv_kernel_size=3, shortcut_kernel_size=1):
+        """
+        Convolutional Layer kernel size Fi 
+        Skip connection (shortcut) kernel size Ki 
+        """
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=conv_kernel_size, stride=stride, padding=int(conv_kernel_size/2), bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=conv_kernel_size,stride=1, padding=int(conv_kernel_size/2), bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(in_planes, self.expansion*planes,kernel_size=shortcut_kernel_size, stride=stride, padding=int(shortcut_kernel_size/2), bias=False),
                 nn.BatchNorm2d(self.expansion*planes)
             )
 
@@ -38,6 +39,73 @@ class BasicBlock(nn.Module):
         out = F.relu(out)
         return out
 
+
+
+def conv1x1(in_channels,
+            out_channels,
+            stride=1,
+            groups=1,
+            bias=False):
+    """
+    Convolution 1x1 layer.
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    stride : int or tuple/list of 2 int, default 1
+        Strides of the convolution.
+    groups : int, default 1
+        Number of groups.
+    bias : bool, default False
+        Whether the layer uses a bias vector.
+    """
+    return nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=1,
+        stride=stride,
+        groups=groups,
+        bias=bias)
+
+class SEBlock(nn.Module):
+    """
+    Squeeze-and-Excitation block from 'Squeeze-and-Excitation Networks,' https://arxiv.org/abs/1709.01507.
+    Parameters:
+    ----------
+    channels : int
+        Number of channels.
+    reduction : int, default 16
+        Squeeze reduction value.
+    """
+    def __init__(self,
+                 channels,
+                 reduction=16):
+        super(SEBlock, self).__init__()
+        mid_cannels = channels // reduction
+
+        self.pool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.conv1 = conv1x1(
+            in_channels=channels,
+            out_channels=mid_cannels,
+            bias=True)
+        self.activ = nn.ReLU(inplace=True) 
+
+        self.conv2 = conv1x1(
+            in_channels=mid_cannels,
+            out_channels=channels,
+            bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        w = self.pool(x)
+        w = self.conv1(w)
+        w = self.activ(w)
+        w = self.conv2(w)
+        w = self.sigmoid(w)
+        x = x * w
+        return x
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -71,38 +139,116 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(
+            self, 
+            block, 
+            num_blocks, 
+            conv_kernel_sizes=None, 
+            shortcut_kernel_sizes=None,
+            num_classes=10, 
+            num_channels=32, 
+            avg_pool_kernel_size=4, 
+            drop=None, 
+            squeeze_and_excitation=None):
         super(ResNet, self).__init__()
-        self.in_planes = 64
+        self.in_planes = num_channels
+        # self.avg_pool_kernel_size = avg_pool_kernel_size 
+        self.avg_pool_kernel_size = int(32 / (2**(len(num_blocks)-1)))
+        
+        """
+        # of channels Ci 
+        """
+        self.num_channels = num_channels
+        self.conv1 = nn.Conv2d(3, self.num_channels, kernel_size=3,stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.num_channels) 
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.drop = drop 
+        self.squeeze_and_excitation = squeeze_and_excitation 
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+        if self.squeeze_and_excitation: 
+            self.seblock = SEBlock(channels=self.num_channels) 
+
+        """
+        # of Residual Layers N 
+        # of Residual Blocks Bi 
+        """
+        self.residual_layers = [] 
+        for n in range(len(num_blocks)): 
+            stride = 1 if n==0 else 2 # stride=1 for first residual layer, and stride=2 for the remaining layers 
+            conv_kernel_size = conv_kernel_sizes[n] if conv_kernel_sizes else 3 # setting default kernel size of block's convolutional layers 
+            shortcut_kernel_size = shortcut_kernel_sizes[n] if shortcut_kernel_sizes else 1 # setting default kernel size of block's skip connection (shortcut) layers 
+            self.residual_layers.append(self._make_layer(
+                                                    block, 
+                                                    self.num_channels*(2**n), 
+                                                    num_blocks[n], 
+                                                    stride=stride, 
+                                                    conv_kernel_size=conv_kernel_size, 
+                                                    shortcut_kernel_size=shortcut_kernel_size)) 
+
+        self.residual_layers = nn.ModuleList(self.residual_layers)
+        self.linear = nn.Linear(self.num_channels*(2**n)*block.expansion, num_classes) 
+        """
+        Dropout layer 
+        """
+        if self.drop: 
+            self.dropout = nn.Dropout(self.drop) # Define proportion or neurons to dropout
+
+    def _make_layer(self, block, planes, num_blocks, stride, conv_kernel_size, shortcut_kernel_size):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, conv_kernel_size, shortcut_kernel_size))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        if self.squeeze_and_excitation: out = self.seblock(out) 
+        for layer in self.residual_layers: 
+            out = layer(out)         
+        """
+        Average pool kernel size 
+        """
+        out = F.avg_pool2d(out, self.avg_pool_kernel_size)
         out = out.view(out.size(0), -1)
+        if self.drop: out = self.dropout(out)
         out = self.linear(out)
         return out
 
+
+def get_ResNet(config): 
+    net =  ResNet(
+            block=BasicBlock, 
+            num_blocks=config['num_blocks'],                          # N: number of Residual Layers | Bi:Residual blocks in Residual Layer i 
+            conv_kernel_sizes=config['conv_kernel_sizes'],            # Fi: Conv. kernel size in Residual Layer i 
+            shortcut_kernel_sizes=config['shortcut_kernel_sizes'],    # Ki: Skip connection kernel size in Residual Layer i 
+            num_channels=config['num_channels'],                      # Ci: # channels in Residual Layer i 
+            avg_pool_kernel_size=config['avg_pool_kernel_size'],      # P: Average pool kernel size 
+            drop=config['drop'],                                      # use dropout with drop proportion 
+            squeeze_and_excitation=config['squeeze_and_excitation']   # Enable/disable Squeeze-and-Excitation Block 
+        ) 
+    
+    total_params = 0 
+    for x in filter(lambda p: p.requires_grad, net.parameters()):
+        total_params += np.prod(x.data.numpy().shape)
+    # print("Total number of params", total_params)
+    # print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size())>1, net.parameters())))) 
+    return net, total_params 
+
+
+def get_ResNet_default():
+    return ResNet(
+        block=BasicBlock, 
+        num_blocks=[2,1,1,1],               # N: number of Residual Layers | Bi:Residual blocks in Residual Layer i 
+        conv_kernel_sizes=[3,3,3,3],        # Fi: Conv. kernel size in Residual Layer i 
+        shortcut_kernel_sizes=[1,1,1,1],    # Ki: Skip connection kernel size in Residual Layer i 
+        num_channels=64,                    # Ci: # channels in Residual Layer i
+        avg_pool_kernel_size=4              # P: Average pool kernel size 
+    ) 
+
+
+def ResNet9():
+    return ResNet(BasicBlock, [2, 1, 1, 1])
 
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
@@ -129,4 +275,15 @@ def test():
     y = net(torch.randn(1, 3, 32, 32))
     print(y.size())
 
-# test()
+def test2():
+    net = get_ResNet_default()
+    total_params = 0 
+    print(net)
+
+    for x in filter(lambda p: p.requires_grad, net.parameters()):
+        total_params += np.prod(x.data.numpy().shape)
+    print("Total number of params", total_params)
+    print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size())>1, net.parameters()))))
+
+# test() 
+# test2() 
